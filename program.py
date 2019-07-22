2ccccc181236eae4981a907ca7e8462b0979c1e8
svandenb080618 <simon.vandenbulcke@socgen.com>
2019-07-22 10:00:05 +0000

    update grid and batman class

--- program.py
+++ program.py
@@ -1,25 +1,69 @@
 import os
+import random
 
 # w: width of the building.
 # h: height of the building.
- # the direction of the bombs from batman's current location (U, UR, R, DR, D,
- # DL, L or UL, Down, Up, Right, Left)
+# the direction of the bombs from batman's current location (U, UR, R, DR, D,
+# DL, L or UL, Down, Up, Right, Left)
 
 w = 4
 h = 4
 
+
 class Gird:
-    '''
+    """
     define Grid
-    '''
-    def __init__(self, width=4, height=4):
+    """
+
+    def __init__(self, width=4, height=4, direction="U", bat_x=2, bat_y=2):
         self.width = width
         self.height = height
+        self.dir = direction
+        self.bat_x = bat_x
+        self.bat_y = bat_y
+        self.white_grid = []
 
     def get(self):
-        return self.width, self.height
-    def set(self, w, h):
-	    self.width = w
-	    self.height = h
+        return self.width, self.height, self.bat_x, self.bat_y
+
+    def set(self, bat_x, bat_y):
+        self.bat_x = bat_x
+        self.bat_y = bat_y
+
+    def generate_white_gird(self):
+        if self.dir == "U":
+            for a in range(self.width):
+                for b in range(self.bat_y, self.height):
+                    self.white_grid.append((a, b))
+            return self.white_grid
+
+        elif self.dir == "D":
+            for a in range(self.width):
+                for b in range(0, self.bat_y):
+                    self.white_grid.append((a, b))
+            return self.white_grid
+        elif self.dir == "L":
+            for a in range(0, self.bat_x - 1):
+                for b in range(self.height):
+                    self.white_grid.append((a, b))
+            return self.white_grid
+        elif self.dir == "R":
+            for a in range(self.bat_x - 1, self.width):
+                for b in range(self.height):
+                    self.white_grid.append((a, b))
+            return self.white_grid
+        else:
+            return self.white_grid
 
 
+class Batman:
+    inital_gird = []
+
+    def __init__(self, correct_gird=[]):
+        self.correct_gird = correct_gird
+        self.jump_place = list(set(self.correct_gird) - set(self.inital_gird))
+        self.random_nb = random.randint(0, len(self.jump_place))
+
+    def jump(self):
+        self.jump = self.jump_place[self.random_nb]
+        return self.jump
--- test_program.py
+++ test_program.py
@@ -1,5 +1,5 @@
 import pytest
-from program import Gird
+from program import Gird, Batman
 
 
 def test_grid_is_instance():
@@ -7,20 +7,119 @@ def test_grid_is_instance():
     assert isinstance(grid, Gird)
 
 
-def test_gird_taken_2_argument():
-    gird = Gird(width=4, height=4)
+def test_batman_is_instance():
+    bat = Batman()
+    assert isinstance(bat, Batman)
+
+
+def test_gird_taken_5_argument():
+    gird = Gird(width=4, height=4, bat_x=4, bat_y=3, direction="R")
     assert gird.width == 4
     assert gird.height == 4
+    assert gird.bat_x == 4
+    assert gird.bat_y == 3
+    assert gird.dir == "R"
 
 
 def test_grid_has_getter_and_return_tuple():
     gird = Gird(4, 5)
-    assert gird.get() == (4, 5)
+    assert gird.get() == (4, 5, 2, 2)
 
 
 def test_gird_has_setter_and_modify_value():
     gird = Gird(4, 6)
     gird.set(3, 5)
-    assert gird.width == 3
-    assert gird.height == 5
+    assert gird.bat_x == 3
+    assert gird.bat_y == 5
 
+
+def test_grid_return_expected_white_gird_if_direction_is_up():
+    gird = Gird(width=4, height=4, bat_x=3, bat_y=2, direction="U")
+    white_gird = gird.generate_white_gird()
+    assert white_gird == [
+        (0, 2),
+        (0, 3),
+        (1, 2),
+        (1, 3),
+        (2, 2),
+        (2, 3),
+        (3, 2),
+        (3, 3),
+    ]
+
+
+def test_grid_return_expected_white_gird_if_direction_is_down():
+    gird = Gird(width=4, height=4, bat_x=3, bat_y=2, direction="D")
+    white_gird = gird.generate_white_gird()
+    assert white_gird == [
+        (0, 0),
+        (0, 1),
+        (1, 0),
+        (1, 1),
+        (2, 0),
+        (2, 1),
+        (3, 0),
+        (3, 1),
+    ]
+
+
+def test_grid_return_expected_white_gird_if_direction_is_left():
+    gird = Gird(width=4, height=4, bat_x=3, bat_y=2, direction="L")
+    white_gird = gird.generate_white_gird()
+    assert white_gird == [
+        (0, 0),
+        (0, 1),
+        (0, 2),
+        (0, 3),
+        (1, 0),
+        (1, 1),
+        (1, 2),
+        (1, 3),
+    ]
+
+
+def test_grid_return_expected_white_gird_if_direction_is_right():
+    gird = Gird(width=4, height=4, bat_x=3, bat_y=2, direction="R")
+    white_gird = gird.generate_white_gird()
+    assert white_gird == [
+        (2, 0),
+        (2, 1),
+        (2, 2),
+        (2, 3),
+        (3, 0),
+        (3, 1),
+        (3, 2),
+        (3, 3),
+    ]
+
+
+def test_batman_take_a_gird():
+    gird = Gird(width=4, height=4, bat_x=3, bat_y=2, direction="R")
+    white_gird = gird.generate_white_gird()
+    bat = Batman(white_gird)
+    assert bat.correct_gird == [
+        (2, 0),
+        (2, 1),
+        (2, 2),
+        (2, 3),
+        (3, 0),
+        (3, 1),
+        (3, 2),
+        (3, 3),
+    ]
+
+
+def test_if_jump_return_is_present_on_white_gird():
+    gird = Gird(width=4, height=4, bat_x=3, bat_y=2, direction="R")
+    white_gird = gird.generate_white_gird()
+    bat = Batman(white_gird)
+    value = bat.jump()
+    assert value in bat.jump_place
+
+def test_if_jump_return_is_expected_value_on_white_gird():
+    gird = Gird(width=4, height=4, bat_x=3, bat_y=2, direction="R")
+    white_gird = gird.generate_white_gird()
+    bat = Batman(white_gird)
+    bat.random_nb = 2
+    value = bat.jump()
+    assert (3, 0) == bat.jump_place[2]
